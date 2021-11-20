945117e304b73570ca8886bd14f8fbd7d8b72076
Лилия Камалтдинова <l.kamaltdinova@innopolis.ru>
2021-11-21 00:45:14 +0500

    Добавлен контекст для тестового контроллера test-context

--- products/views.py
+++ products/views.py
@@ -9,3 +9,20 @@ def index(request):
 
 def products(request):
     return render(request, 'products/products.html')
+
+
+def test_context(request):
+    # context = content, мы формируем контекст (контент) и передаем его в шаблон
+    # формируем данные в контроллере и передаем в шаблон
+    context = {
+        'title': 'GeekShop',
+        'header': 'Добро пожаловать на сайт!',
+        'user': 'Иван Иванов',
+        'products': [
+            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 6090},
+            {'name': 'Синяя куртка The North Face', 'price': 23725},
+            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': 3390},
+        ],
+        'is_promotion': True,
+    }
+    return render(request, 'products/test-context.html', context)
